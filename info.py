465a6461526766a9f02244bb571aab520007d7a7
Kamalesh9787 <kamaleshking957@gmail.com>
2025-01-18 14:07:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "initial-fanchette-mlp-auto-b0fd4ff9.koyeb.app/")
+URL = environ.get("URL", "https://initial-fanchette-mlp-auto-b0fd4ff9.koyeb.app/")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
